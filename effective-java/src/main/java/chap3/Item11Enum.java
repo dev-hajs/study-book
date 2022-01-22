72a4130462c9db428578acf483e4c14099d74028
하지수 <dev.hajs@gmail.com>
2022-01-22 14:35:50 +0900

    item11 - create enum

---
+++ effective-java/src/main/java/chap3/Item11Enum.java
@@ -0,0 +1,16 @@
+package chap3;
+
+public enum Item11Enum {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY;
+
+
+
+//    public String toString() {
+//        return name().charAt(0) + name().substring(1).toLowerCase();
+//    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
