3c55fdcd818c3f7d2ed3df06a1b793584be42dbd
하지수 <dev.hajs@gmail.com>
2022-04-17 16:05:26 +0900

    Item 71

--- effective-java/src/main/java/chap10/Item71.java
+++ effective-java/src/main/java/chap10/Item71.java
@@ -2,18 +2,25 @@ package chap10;
 
 public class Item71 {
 
-
     public static void main(String[] args) {
         System.out.println("start");
 
-        try {
-            System.out.println("try");
-            throw new TheCheckedException("bamm!");
-        } catch (TheCheckedException e) {
-            throw new AssertionError();
-        } finally {
-            System.out.println("end");
-        }
+        action();
 
+        System.out.println("end");
+    }
+
+    public static void action() {
+        try {
+
+            System.out.println("> try");
+            throw new TheCheckedException("bamm!");
+
+        } catch (TheCheckedException e) {
+
+            System.out.println("> catch");
+            throw new AssertionError("bamm!");
+
+        }
     }
 }
