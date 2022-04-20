aaeeff941a0a24c4d862a8eddd678001fe2cce27
하지수 <dev.hajs@gmail.com>
2022-04-20 20:42:30 +0900

    Item81 - after refactoring

--- effective-java/src/main/java/chap11/Item81.java
+++ effective-java/src/main/java/chap11/Item81.java
@@ -15,12 +15,39 @@ public class Item81 {
         return previousValue == null ? s : previousValue;
     }
 
+    /**
+     * after refactoring
+     */
+    public static String intern2(String s) {
+        String result = map.get(s);
+        if (result == null) {
+            result = map.putIfAbsent(s, s);
+            if (result == null) {
+                result = s;
+            }
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         String str = "super";
+
+        System.out.println("intern = " + intern);
+        System.out.println("* ========== *");
+
         String internStr = Item81.intern(str);
         System.out.println("str: " + str);
         System.out.println("internStr: " + internStr);
         System.out.println("str == internStr: " + (str == internStr));
         System.out.println("str.equals(internStr): " + (str.equals(internStr)));
+
+        System.out.println("* ========== *");
+
+        String internStr2 = Item81.intern2(str);
+        System.out.println("str: " + str);
+        System.out.println("internStr2: " + internStr2);
+        System.out.println("str == internStr2: " + (str == internStr2));
+        System.out.println("str.equals(internStr2): " + (str.equals(internStr2)));
     }
 }
