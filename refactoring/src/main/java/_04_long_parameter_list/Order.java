11d6bd338d15a9178b4d26a00551f8ef7028880a
하지수 <dev.hajs@gmail.com>
2022-06-07 20:02:46 +0900

    before the long parameter list

---
+++ refactoring/src/main/java/_04_long_parameter_list/Order.java
@@ -0,0 +1,22 @@
+package _04_long_parameter_list;
+
+public class Order {
+    private int quantity;
+
+    private double itemPrice;
+
+    public Order(int quantity, double itemPrice) {
+        this.quantity = quantity;
+        this.itemPrice = itemPrice;
+    }
+
+    public double finalPrice() {
+        double basePrice = this.quantity * this.itemPrice;
+        int discountLevel = this.quantity > 100 ? 2 : 1;
+        return this.discountedPrice(basePrice, discountLevel);
+    }
+
+    private double discountedPrice(double basePrice, int discountLevel) {
+        return discountLevel == 2 ? basePrice * 0.9 : basePrice * 0.95;
+    }
+}
