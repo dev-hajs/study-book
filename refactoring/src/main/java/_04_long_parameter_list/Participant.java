46d0b13b79a3088fef0db6a75f33cf15e6f48829
하지수 <dev.hajs@gmail.com>
2022-06-08 21:52:26 +0900

    refec: combine functions into class

---
+++ refactoring/src/main/java/_04_long_parameter_list/Participant.java
@@ -0,0 +1,14 @@
+package _04_long_parameter_list;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public record Participant(String username, Map<Integer, Boolean> homework) {
+    public Participant(String username) {
+        this(username, new HashMap<>());
+    }
+
+    public void setHomeworkDone(int index) {
+        this.homework.put(index, true);
+    }
+}
