553a1e9a046a1cad96ec39815436cd25aa72a92f
하지수 <dev.hajs@gmail.com>
2022-06-19 14:22:04 +0900

    refec: move function

---
+++ refactoring/src/main/java/_07_divergent_change/Account.java
@@ -0,0 +1,20 @@
+package _07_divergent_change;
+
+public class Account {
+
+    private int daysOverdrawn;
+
+    private AccountType type;
+
+    public double getBankCharge() {
+        double result = 4.5;
+        if (this.daysOverdrawn > 0) {
+            result += this.type.overdraftCharge(this.daysOverdrawn);
+        }
+        return result;
+    }
+
+    private int daysOverdrawn() {
+        return this.daysOverdrawn;
+    }
+}
