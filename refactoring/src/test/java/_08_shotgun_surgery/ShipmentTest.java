3b351214a1b5c8c0bcf3d87b42ce3bfe68e9b268
하지수 <dev.hajs@gmail.com>
2022-06-21 22:05:25 +0900

    refec: inline class

---
+++ refactoring/src/test/java/_08_shotgun_surgery/ShipmentTest.java
@@ -0,0 +1,15 @@
+package _08_shotgun_surgery;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShipmentTest {
+
+    @Test
+    void trackingInfo() {
+        Shipment shipment = new Shipment("UPS", "12345");
+        assertEquals("UPS: 12345", shipment.getTrackingInfo());
+    }
+
+}
