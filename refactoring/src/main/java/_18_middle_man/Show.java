c80b539b73783ee775aaa95da018136fc970457f
하지수 <dev.hajs@gmail.com>
2022-07-07 01:20:35 +0900

    refec: replace subclass with delegate

---
+++ refactoring/src/main/java/_18_middle_man/Show.java
@@ -0,0 +1,23 @@
+package _18_middle_man;
+
+import java.util.List;
+
+public class Show {
+
+    private List<String> properties;
+
+    private double price;
+
+    public Show(List<String> properties, double price) {
+        this.properties = properties;
+        this.price = price;
+    }
+
+    public boolean hasOwnProperty(String property) {
+        return this.properties.contains(property);
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
